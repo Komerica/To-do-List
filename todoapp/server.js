64b6e878f120210b8e2d7891f18b39ce519ded53
Kosy <jepilworld@gmail.com>
2022-12-24 23:49:05 +1100

    REST API + Connect to MongoDB

--- todoapp/server.js
+++ todoapp/server.js
@@ -5,7 +5,20 @@
 //          app.listen();
 const express = require('express');
 const app = express();
-// ↓ 내 컴퓨터에 서버를 열수 있게 해줌  // ↓ 콜백함수를 왜 쓰냐면.. 자바스크립트에서 뭔가 순차적으로 실행하고 싶을 때 사용한다고만 알면 됨.                                                     listen() 함수를 동작시킨 다음, function(){} 내에 있는 코드를 실행해주세요~ 라는 뜻으로 사용한 것!                                               Node.js 특성상 코드를 연달아서 2개 적는다고 그 코드가 순차적으로 실행된다는 보장 X (이전강의 참고)                                                뭔가 순차적으로 실행할 때 '함수안에 함수를 집어넣는 콜백함수'를 꼭 사용합니다.
+// 🟧 2. 아래와 같이 적어줌! (이것까지 해줘야 body-parser 을 사용할 수 있음!)
+const bodyParser = require('body-parser');          // body-parser 을 쓰기 위해 추가
+app.use(bodyParser.urlencoded({extended: true}));   // body-parser 을 쓰기 위해 추가
+// 🟪 1. DB 연결(MongoDB)
+//       npm install mongodb    ← mongodb 설치
+const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb+srv://admin:<qwer1234>@cluster0.2j7fqtv.mongodb.net/?retryWrites=true&w=majority', function (error, client) {
+
+});
+
+// ↓ 내 컴퓨터에 서버를 열수 있게 해줌  // ↓ 콜백함수를 왜 쓰냐면.. 자바스크립트에서 뭔가 순차적으로 실행하고 싶을 때 사용한다고만 알면 됨
+                                   //   listen() 함수를 동작시킨 다음, function(){} 내에 있는 코드를 실행해주세요~ 라는 뜻으로 사용한 것!
+                                   //   Node.js 특성상 코드를 연달아서 2개 적는다고 그 코드가 순차적으로 실행된다는 보장 X (이전강의 참고)
+                                   //   뭔가 순차적으로 실행할 때 '함수안에 함수를 집어넣는 콜백함수'를 꼭 사용합니다.
 app.listen(8080, function () {
          // ↑ 서버를 오픈할 포트번호  ↑ 서버 오픈시 실행할 코드
     console.log('listening on 8080');
@@ -24,8 +37,29 @@ app.get('/pet', function (req, res) {
 app.get('/beauty', function (req, res) {
     res.send('Welcome to my beauty shop!');
 });
-//      '/': 메인 홈페이지 (localhost:8080)
+//    ↓ '/': 메인 홈페이지 (localhost:8080)
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/write.html');     // sendFile: html 파일을 보낼 수 있다!
+    res.sendFile(__dirname + '/index.html');     // sendFile: html 파일을 보낼 수 있다!
                    // __dirname: directory name
 });
+app.get('/write', function (req, res) {
+    res.sendFile(__dirname + '/write.html');
+});
+// 어떤 사람이 /add 경로로 POST 요청을 하면, ??를 해주세요!
+app.post('/newpost', function (req, res) {
+    res.send('Sent!');
+    console.log(req.body.toDo); // 🟧 3. 내가 방금 전달한 데이터가 console 창에 뜸! (toDo = input 에 있는 name 속성)
+    console.log(req.body);      // POST 요청을 할때 거기 들어가 있는 정보들! => { toDo: 'Coding', date: '2023-01-03' }  ← Object 자료형
+    console.log(req.body.date); // 2023-01-03
+    // 영구저장하고 싶으면 여기다가 "DB에 저장해주세요" 라고 코드짜면 됨
+});
+// ↑ ↑ 우리가 이제 영구 저장할 정보들(write.html 에서 적은 정보들)은 어디로 저장되는 것일까? -> 콜백함수의 req 에 들어가 있음
+//     하지만 이것을 req 에서 꺼내 쓰려면 또 다른 library 가 필요! -> body-parser
+//  🟧 POST 요청으로 input 에 있는 데이터를 서버에 전송하고 싶으면..
+//     1. npm install body-parser   라이브러리 설치
+//        *body-parser: input 안에 적힌 데이터를 해석할 수 있도록 도와주는 라이브러리!
+//                      이 라이브러리 없으면 req.body.toDo / req.body.date 이런거 못씀!
+
+
+
+
